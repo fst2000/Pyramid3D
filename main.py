b8404e2b88f971ab08615c3cbdfc2ced14b19090
fst2000 <fst.18.work@gmail.com>
2024-07-16 20:45:54 +0400

    dot functions fixed

--- main.py
+++ main.py
@@ -4,39 +4,37 @@ from operator import mul
 import glfw
 from OpenGL.GL import *
 
-def vec2(x, y): return {"x": x, "y": y}
-
-def vec3(x, y, z): return {"x": x, "y": y, "z": z}
-
-def length(vec): return math.sqrt(sum(map(lambda i: i * i), vec.values()))
+def length(vec): return math.sqrt(sum(map(lambda i: i * i), vec))
 
 def angle(v1, v2): return math.acos(dot_vec(v1, v2) / length(v1) * length(v2))
 
-def dot_vec(v1, v2): return sum(map(lambda a1, a2: a1 * a2, v1.values(), v2.values()))
+def dot_vec(v1, v2): return sum(map(lambda a1, a2: a1 * a2, v1, v2))
 
 def matrix(size : int): return enumerated_matrix(lambda i, j: 1 if i == j else 0, size)
 
 def enumerated_matrix(function, size : int): return tuple(tuple(function(i, j) for j in range(size)) for i in range(size))
 
-def map_matrix(function, matrix): return list(map(lambda row: list(map(lambda a: function(a), row)), matrix))
+def map_matrix(function, matrix): return tuple(map(lambda row: tuple(map(lambda a: function(a), row)), matrix))
 
 def transpose(m): return enumerated_matrix(lambda i, j: m[j[i]])
 
-def column(matrix, idx : int): return list(map(lambda row: row[idx], matrix))
+def column(matrix, idx : int): return tuple(map(lambda row: row[idx], matrix))
 
-def dot_mat(m1, m2): return [dot_vec(row, column(m2, idx)) for idx, row in enumerate(m1)]
+def dot_mat(m1, m2): return tuple(enumerated_matrix(lambda i, j: dot_vec(m1[i], column(m2, j)), len(m1)))
 
-def y_rotation_matrix3(angle): return [
+def dot_mat_vec(m, v): return tuple(dot_vec(row, v) for row in m)
+
+def y_rot_mat(angle): return [
     [math.cos(angle), 0, math.sin(angle)],
     [0, 1, 0],
     [-math.sin(angle), 0, math.cos(angle)]
     ]
 
-def draw():
+def draw(angle : float):
     glBegin(GL_TRIANGLES)
-    glVertex3f(0, 0.5, 0)
-    glVertex3f(0.5, -0.5, 0)
-    glVertex3f(-0.5, -0.5, 0)
+    vecs = ((0, 0.5, 0), (0.5, -0.5, 0), (-0.5, -0.5, 0))
+    rot_mat = y_rot_mat(angle)
+    for vec in map(lambda vec: dot_mat_vec(rot_mat, vec), vecs): glVertex(*vec)
     glEnd()
 
 def start_window():
@@ -51,9 +49,11 @@ def start_window():
 def main():
     glfw.init()
     window = start_window()
+    angle = 30.0
     while not glfw.window_should_close(window):
         glfw.poll_events()
-        draw()
+        draw(angle)
+        angle += 0.01
         glfw.swap_buffers(window)
     glfw.terminate()
 
