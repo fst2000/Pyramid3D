5fa51fd4a882f6892a71b8eef9fd4a6498e9fe46
fst2000 <fst.18.work@gmail.com>
2024-07-23 13:53:19 +0400

    obj parsing added

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ def angle(v1, v2): return math.acos(dot_vec(v1, v2) / length(v1) * length(v2))
 
 def dot_vec(v1, v2): return sum(map(lambda a1, a2: a1 * a2, v1, v2))
 
+def sum_vec(*vecs): return tuple(sum(values) for values in zip(*vecs))
+
+def scale_vec(vec, scale): return tuple(map(lambda value: value * scale))
+
 def cross(v1, v2): pass
+
+def vertex(point, texcoord, normal): return {"point" : point, "texcoord": texcoord, "normal": normal}
 
 def matrix(size : int): return enumerated_matrix(lambda i, j: 1 if i == j else 0, size)
 
@@ -32,14 +38,25 @@ def y_rot_mat(angle): return [
     [-math.sin(angle), 0, math.cos(angle)]
     ]
 
-def transform_face(matrix, face): return tuple(map(lambda v: dot_mat_vec(matrix, v), face))
+def transform_face(matrix, face): return tuple(map(lambda v: vertex(dot_mat_vec(matrix, v["point"]), v["texcoord"], v["normal"]), face))
 
 def transform_mesh(matrix, mesh): return tuple(map(lambda f: transform_face(matrix, f), mesh))
 
+def mesh_from_obj(obj_file):
+    lines = obj_file.split("\n")
+    filtered_lines = lambda s: tuple(map(lambda line: line.replace(s, ""), filter(lambda line: line.startswith(s), lines)))
+    floats_from_string = lambda l: tuple(map(lambda val: float(val), l.split()))
+    points = tuple(map(floats_from_string, filtered_lines("v ")))
+    texcoords = tuple(map(floats_from_string, filtered_lines("vt ")))
+    normals = tuple(map(floats_from_string, filtered_lines("vn ")))
+    vertex_from_ids = lambda v_id, vt_id, vn_id: vertex(points[v_id], texcoords[vt_id], normals[vn_id])
+    face_from_line = lambda line: tuple(map(lambda ids_str: vertex_from_ids(*map(lambda id_str: int(id_str) - 1, ids_str.split("/"))), line.split()))
+    return tuple(face_from_line(line) for line in filtered_lines("f "))
+
 def draw_face(face):
-    for vec in face:
-        glColor3f(*vec)
-        glVertex(*vec)
+    for vertex in face:
+        glColor3f(*vertex["normal"])
+        glVertex(*vertex["point"])
 
 def normal(face):
     return
@@ -62,10 +79,7 @@ def main():
     glfw.init()
     window = start_window()
     angle = 0.0
-    pyramid_mesh = (((0, 0.5, 0), (0.5, -0.5, 0.5), (-0.5, -0.5, 0.5)),
-                     ((0, 0.5, 0), (0.5, -0.5, -0.5), (-0.5, -0.5, -0.5)),
-                     ((0, 0.5, 0), (0.5, -0.5, -0.5), (0.5, -0.5, 0.5)),
-                     ((0, 0.5, 0), (-0.5, -0.5, 0.5), (-0.5, -0.5, 0.5)))
+    pyramid_mesh = mesh_from_obj(open("pyramid.obj").read())
     while not glfw.window_should_close(window):
         glfw.poll_events()
         glClear(GL_COLOR_BUFFER_BIT)
@@ -73,5 +87,4 @@ def main():
         angle += 0.001
         glfw.swap_buffers(window)
     glfw.terminate()
-
 main()
