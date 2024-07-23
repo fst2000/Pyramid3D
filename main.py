77b7732d81c732c8bcc2d85d9dc6000ee99fa50b
fst2000 <fst.18.work@gmail.com>
2024-07-23 17:16:21 +0400

    camera perspective and texture

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import math
 from functools import reduce
 from operator import mul
 import glfw
-
+import numpy as np
 from OpenGL.GLUT import *
+from OpenGL.GLU import *
 from OpenGL.GL import *
+from PIL import Image
 
 def length(vec): return math.sqrt(sum(map(lambda i: i * i), vec))
 
@@ -15,6 +17,8 @@ def dot_vec(v1, v2): return sum(map(lambda a1, a2: a1 * a2, v1, v2))
 def sum_vec(*vecs): return tuple(sum(values) for values in zip(*vecs))
 
 def scale_vec(vec, scale): return tuple(map(lambda value: value * scale, vec))
+
+def inverse_vec(vec): return scale_vec(vec, -1)
 
 def average_vec(*vecs): return scale_vec(sum_vec(vecs), len(vecs))
 
@@ -49,6 +53,14 @@ def transform_face(matrix, face): return tuple(map(lambda v: vertex(dot_mat_vec(
 
 def transform_mesh(matrix, mesh): return tuple(map(lambda f: transform_face(matrix, f), mesh))
 
+def load_texture(image):
+    image = image.convert('RGB')
+    image_data = np.array(list(image.getdata()), np.uint8)
+    texture_id = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
+    return texture_id
+
 def mesh_from_obj(obj_file):
     lines = obj_file.split("\n")
     filtered_lines = lambda s: tuple(map(lambda line: line.replace(s, ""), filter(lambda line: line.startswith(s), lines)))
@@ -63,19 +75,21 @@ def mesh_from_obj(obj_file):
 def draw_face(face, vertex_col_func):
     for vertex in face:
         glColor3f(*vertex_col_func(vertex))
+        glTexCoord2f(*vertex["texcoord"])
         glVertex(*vertex["point"])
 
-def normal(face):
-    return
-
-def draw(mesh, vertex_col_func):
+def draw(mesh, vertex_col_func, texture_id):
+    glBindTexture(GL_TEXTURE_2D, texture_id)
     glBegin(GL_TRIANGLES)
     for face in mesh: draw_face(face, vertex_col_func)
     glEnd()
 
 def start_window():
-    window = glfw.create_window(1000, 1000, "draw_test", None, None)
+    w = 1000
+    h = 1000
+    window = glfw.create_window(w, h, "draw_test", None, None)
     glfw.make_context_current(window)
+    gluPerspective(45.0, float(w) / float(h), 0.1, 50.0)
     if not window:
         glfw.terminate()
         print("Glfw window can't be created")
@@ -86,15 +100,21 @@ def main():
     glfw.init()
     window = start_window()
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_TEXTURE_2D)
+    cam_pos = (0, 0, -3)
+    cam_dir = (0, 0, 1)
+    gluLookAt(*cam_pos, *sum_vec(cam_pos, cam_dir), 0.0, 1.0, 0.0)
     angle = 0.0
-    pyramid_mesh = mesh_from_obj(open("papa.obj").read())
+    mesh = mesh_from_obj(open("papa.obj").read())
+    texture_id = load_texture(Image.open("skin_winter.png"))
     while not glfw.window_should_close(window):
         glfw.poll_events()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         transform_matrix = y_rot_mat(angle)
         draw(transform_mesh(
-            transform_matrix, pyramid_mesh),
-            lambda vertex: scale_vec((1.0, 1.0, 1.0), dot_vec(dot_mat_vec(transform_matrix, vertex["normal"]), (0.0, 0.0, -1.0))))
+            transform_matrix, mesh),
+            lambda vertex: scale_vec((1.0, 1.0, 1.0), dot_vec(dot_mat_vec(transform_matrix, vertex["normal"]), inverse_vec(cam_dir))),
+            texture_id)
         angle += 0.01
         glfw.swap_buffers(window)
     glfw.terminate()
