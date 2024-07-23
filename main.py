7372f4daedeecf4a4c09111ed0e4a44999eed966
fst2000 <fst.18.work@gmail.com>
2024-07-23 14:38:34 +0400

    papa

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def dot_vec(v1, v2): return sum(map(lambda a1, a2: a1 * a2, v1, v2))
 
 def sum_vec(*vecs): return tuple(sum(values) for values in zip(*vecs))
 
-def scale_vec(vec, scale): return tuple(map(lambda value: value * scale))
+def scale_vec(vec, scale): return tuple(map(lambda value: value * scale, vec))
 
 def average_vec(*vecs): return scale_vec(sum_vec(vecs), len(vecs))
 
@@ -34,12 +34,15 @@ def column(matrix, idx : int): return tuple(map(lambda row: row[idx], matrix))
 
 def dot_mat(m1, m2): return tuple(enumerated_matrix(lambda i, j: dot_vec(m1[i], column(m2, j)), len(m1)))
 
-def dot_mat_vec(m, v): return tuple(dot_vec(row, v) for row in m)
+def dot_mat_vec(m, v):
+    if len(v) < len(m): v = v + ((0,) * (len(m) - len(v) - 1) + (1, ))
+    return tuple(dot_vec(row, v) for row in m)
 
 def y_rot_mat(angle): return [
-    [math.cos(angle), 0, math.sin(angle)],
-    [0, 1, 0],
-    [-math.sin(angle), 0, math.cos(angle)]
+    [math.cos(angle), 0, math.sin(angle), 0],
+    [0, 1, 0, 0],
+    [-math.sin(angle), 0, math.cos(angle), 0],
+    [0, 0, 0, 1]
     ]
 
 def transform_face(matrix, face): return tuple(map(lambda v: vertex(dot_mat_vec(matrix, v["point"]), v["texcoord"], v["normal"]), face))
@@ -57,17 +60,17 @@ def mesh_from_obj(obj_file):
     face_from_line = lambda line: tuple(map(lambda ids_str: vertex_from_ids(*map(lambda id_str: int(id_str) - 1, ids_str.split("/"))), line.split()))
     return tuple(face_from_line(line) for line in filtered_lines("f "))
 
-def draw_face(face):
+def draw_face(face, vertex_col_func):
     for vertex in face:
-        glColor3f(*vertex["normal"])
+        glColor3f(*vertex_col_func(vertex))
         glVertex(*vertex["point"])
 
 def normal(face):
     return
 
-def draw(mesh):
+def draw(mesh, vertex_col_func):
     glBegin(GL_TRIANGLES)
-    for face in mesh: draw_face(face)
+    for face in mesh: draw_face(face, vertex_col_func)
     glEnd()
 
 def start_window():
@@ -84,12 +87,15 @@ def main():
     window = start_window()
     glEnable(GL_DEPTH_TEST)
     angle = 0.0
-    pyramid_mesh = mesh_from_obj(open("pyramid.obj").read())
+    pyramid_mesh = mesh_from_obj(open("papa.obj").read())
     while not glfw.window_should_close(window):
         glfw.poll_events()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        draw(transform_mesh(y_rot_mat(angle), pyramid_mesh))
-        angle += 0.001
+        transform_matrix = y_rot_mat(angle)
+        draw(transform_mesh(
+            transform_matrix, pyramid_mesh),
+            lambda vertex: scale_vec((1.0, 1.0, 1.0), dot_vec(vertex["normal"], dot_mat_vec(transform_matrix, (0.0, 0.0, -1.0)))))
+        angle += 0.01
         glfw.swap_buffers(window)
     glfw.terminate()
 main()
